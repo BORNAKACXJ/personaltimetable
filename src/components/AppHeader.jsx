47694c90f3769d02ff375b164e2340374494b9fa
Joran Backx <joranbackx@Mac.home>
2025-08-27 19:55:08 +0200

    Update AppHeader components and styling

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -19,7 +19,9 @@ export function AppHeader({ isPersonalTimetable, currentUserId, currentUserName,
 
       <div className="section__margin">
         <div className="festival__logo--header">
+          <a href="https://hitthecity-festival.nl/">
           <img src="/_assets/_images/logo-hitthecity.png" alt="Hit the City" />
+          </a>
         </div>
 
 
@@ -44,9 +46,9 @@ export function AppHeader({ isPersonalTimetable, currentUserId, currentUserName,
           // Show main navigation menu when there's no personal timetable
           <div className="nav navbar-nav navbar-main navbar-nav-first">
             <ul id="menu-hoofdmenu" className="menu-primary-inner menu-smart sm" role="menu" data-smartmenus-id="17551600275673013">
-              <li role="menuitem" id="menu-item-3595" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-home menu-item-3595 menu-item-link">
+              {/* <li role="menuitem" id="menu-item-3595" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-home menu-item-3595 menu-item-link">
                 <a href="https://hitthecity-festival.nl/" className="cursor-init">Home</a>
-              </li>
+              </li> */}
               <li role="menuitem" id="menu-item-3589" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-3589 menu-item-link">
                 <a href="timetable.hitthecity-festival.nl" className="cursor-init">Timetable</a>
               </li>
@@ -128,7 +130,7 @@ export function AppHeader({ isPersonalTimetable, currentUserId, currentUserName,
             }}
           >
             <img src="/_assets/_images/spotify_icon.svg" alt="Spotify" style={{ width: '20px', height: '20px' }} />
-            Create <span>your personal </span> timetable
+            <span>Create your personal timetable</span>
           </button>
         )}
       </div>
